3ae37a330f1704f578700fd4d01a9d102b144eda
Diego Moir <diegojmoir@gmail.com>
2018-05-01 23:01:58 -0600

    Nothing important

---
+++ Turing_Machine_Simulator/Turing_Machine_Simulator/Turing Machine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turing_Machine_Simulator
+{
+    class Turing_Machine
+    {
+        public string tape;
+        public string[] alphabet;
+      
+              
+        
+    }
+}
